247c8f227696b84df64ed7de85bc59be802a9863
SujayAmberkar <sujayamberkar@gmail.com>
2021-04-11 14:14:39 +0530

    final final

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from im import img
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI']='DATABASE_URL'
 db = SQLAlchemy(app)
 
 class Painter(db.Model):
